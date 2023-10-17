908507740e3d964fdc51325a6c4df37f44d8c539
josemello79 <129752489+josemello79@users.noreply.github.com>
2023-10-17 12:11:20 +0200

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -4,8 +4,14 @@ from selenium import webdriver
 
 @pytest.fixture
 def login_driver():
-    url = "https://identity.hudl.com/login"
+    base_url = "https://identity.hudl.com/login?"
+    state = ("state"
+             "=hKFo2SA0OGRRVmtiOGRxTm5GRmh5VlBzaTlaZHAyRVpvUlZHcaFupWxvZ2luo3RpZNkgZjBoYnNOQnIweF8yNGRPNHRvMkFCdVZCOUpScVVyYjmjY2lk2SBuMTNSZmtIektvemFOeFdDNWRaUW9iZVdHZjRXalNuNQ")
+    client = "&client=n13RfkHzKozaNxWC5dZQobeWGf4WjSn5"
+    protocol = "&protocol=oauth2"
+    response = "&response_type=id_token"
+    full_login_url = base_url + state + client + protocol + response
     driver = webdriver.Chrome()
-    driver.get(url)
+    driver.get(full_login_url)
     yield driver
     driver.close()

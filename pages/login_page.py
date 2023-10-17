cf247a98dd91b7485f473bd2cc587bc58619b6b5
josemello79 <129752489+josemello79@users.noreply.github.com>
2023-10-17 17:02:56 +0200

    Update login_page.py

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,4 +1,6 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.support.wait import WebDriverWait
 
 from .base_page import BasePage
 
@@ -11,7 +13,7 @@ class LoginPage(BasePage):
     EMAIL_INPUT = (By.ID, "email")
     PASSWORD_INPUT = (By.ID, "password")
     CONTINUE_BUTTON = (By.ID, "logIn")
-    # ERROR_LOCATOR = "undefined_text"
+    ERROR_LOCATOR = (By.XPATH, '//p[@data-qa-id="undefined-text"]')
 
     def enter_login_email(self, email):
         self.wait_for(self.EMAIL_INPUT).send_keys(email)
@@ -21,3 +23,19 @@ class LoginPage(BasePage):
 
     def click_continue_button(self):
         self.wait_for(self.CONTINUE_BUTTON).click()
+
+    def assert_incorrect_login_message(self, value):
+        """
+        Asserts that an error message element is displayed on a web page with the expected text value.
+
+        :param value: The expected text value of the error message element.
+        """
+        try:
+            element = WebDriverWait(self.driver, 10).until(
+                ec.visibility_of_element_located(self.ERROR_LOCATOR)
+            )
+            assert element.is_displayed(), f"Error message element is not displayed: {value}"
+            assert element.text == value
+        except:
+            assert False, f"Error message element not found: {self.ERROR_LOCATOR}"
+

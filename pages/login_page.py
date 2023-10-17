4af2345bf82a925544273b1e093520ca87c779f2
josemello79 <129752489+josemello79@users.noreply.github.com>
2023-10-17 11:41:55 +0200

    Create login_page.py

---
+++ pages/login_page.py
@@ -0,0 +1,23 @@
+from selenium.webdriver.common.by import By
+
+from base_page import BasePage
+
+
+class LoginPage(BasePage):
+    """
+    Login Page for HUDL.
+    """
+
+    EMAIL_INPUT = (By.ID, "email")
+    PASSWORD_INPUT = (By.ID, "password")
+    CONTINUE_BUTTON = (By.ID, "logIn")
+    # ERROR_LOCATOR = "undefined_text"
+
+    def enter_login_email(self, email):
+        self.wait_for(self.EMAIL_INPUT).send_keys(email)
+
+    def enter_login_password(self, password):
+        self.wait_for(self.PASSWORD_INPUT).send_keys(password)
+
+    def click_continue_button(self):
+        self.wait_for(self.CONTINUE_BUTTON).click()

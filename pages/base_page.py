80ebcecfb00c9666e62160ae012fa7a0e8fff804
josemello79 <129752489+josemello79@users.noreply.github.com>
2023-10-17 12:57:15 +0200

    Update base_page.py

--- pages/base_page.py
+++ pages/base_page.py
@@ -14,3 +14,23 @@ class BasePage:
 
     def wait_for(self, locator):
         return self._wait.until(ec.presence_of_element_located(locator))
+
+    @staticmethod
+    def assert_url(driver, expected_url, timeout=10):
+        """
+        Wait for the actual URL to match the expected URL and assert.
+
+        :param driver: The Selenium WebDriver instance.
+        :param expected_url: The expected URL to compare against.
+        :param timeout: Maximum time (in seconds) to wait for the URL to match.
+        :raises AssertionError: If the actual URL does not match the expected URL within the timeout.
+        """
+        try:
+            WebDriverWait(driver, timeout).until(ec.url_to_be(expected_url))
+        except:
+            pass
+
+        actual_url = driver.current_url
+        assert (
+            actual_url == expected_url
+        ), f"Expected URL: {expected_url}, Actual URL: {actual_url}"
